a584a9245570ab4f1f930b5925668c5a74ed6567
unknown <hung.nt207608@sis.hust.edu.vn>
2024-10-10 17:01:02 +0700

    .

--- Assets/UIBeginner/ToolTipProject/ConfigView.cs
+++ Assets/UIBeginner/ToolTipProject/ConfigView.cs
@@ -1,13 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class ConfigView : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    UIDocument uIDocument;
+    VisualElement root;
+    private void Awake() 
     {
-        
+        uIDocument = GetComponent<UIDocument>();
+        root = uIDocument.rootVisualElement;
+        AddDummyDropdownValue();
+    }
+
+    public void AddDummyDropdownValue()
+    {
+        DropdownField dropdown = root.Q<DropdownField>();
+        dropdown.choices.Add("English");
+        dropdown.choices.Add("Tiếng Việt");
+        dropdown.choices.Add("日本語");
+        dropdown.choices.Add("Español");
+        dropdown.choices.Add("Português");
+        dropdown.Query<Label>("unity-base-dropdown__label").ForEach(label => 
+        {
+            label.style.fontSize = 48;
+        });
     }
 
     // Update is called once per frame
